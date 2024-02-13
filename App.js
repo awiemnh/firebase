68987e474e4a8ae49c64791b8571ee77c7bef8bc
Alfan Azizi <76636713+alfanazizi@users.noreply.github.com>
2024-02-13 09:31:52 +0700

    add app

--- App.js
+++ App.js
@@ -1,9 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useEffect } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-
+import { StatusBar } from 'expo-status-bar';
+import { FirebaseCrashlyticsTypes } from '@react-native-firebase/crashlytics';
+import { firebase } from '@react-native-firebase/crashlytics';
+import firebase from 'react-native-firebase/app';
 
 export default function App() {
+  useEffect(() => {
+    firebase.crashlytics().log('App mounted.');
+  }, []);
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
@@ -15,7 +22,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#fff56',
     alignItems: 'center',
     justifyContent: 'center',
   },
